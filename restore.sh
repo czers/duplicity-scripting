c68f9265f3267167808ffdd7c4989514ead3b6cf
Michał Czerski <mczers@gmail.com>
2016-03-22 19:58:56 +0100

    Improved the restore script

--- restore.sh
+++ restore.sh
@@ -1,4 +1,29 @@
 #!/bin/bash
-duplicity \
-    --file-to-restore bbb/ \
-    file://testing-field/backups testing-field/restored-data
+
+DEVICE_NAME=y580
+BACKUP_LOCATION=file:///media/czers/EXTDATA/Backups
+
+source sensitive.sh
+export PASSPHRASE=$BACKUP_PASSPHRASE
+
+function restore () {
+    local symbolic_name=$1
+    local dir_name=$2
+
+    # The following will most probably throw lots of errors related to
+    # restoring ownership
+    duplicity --name "$DEVICE_NAME-$symbolic_name" \
+        $BACKUP_LOCATION/$DEVICE_NAME/$symbolic_name $dir_name
+}
+
+restore Dokumenty "~/restoring-test"
+
+# sudo may be required for restoring ownership metadata:
+# sudo -E \
+#     duplicity --name "$DEVICE_NAME-$symbolic_name" \
+#     $BACKUP_LOCATION/$DEVICE_NAME/$symbolic_name $dir_name
+
+# Narrowed-down restore example:
+# duplicity \
+#     --file-to-restore bbb/ \
+#     file://testing-field/backups testing-field/restored-data
